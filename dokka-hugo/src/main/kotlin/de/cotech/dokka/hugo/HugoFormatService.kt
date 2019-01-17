5f83e95ff06d03ed80af08680dcbbd3ee79d8ca3
Dominik Schürmann <dominik@schuermann.eu>
2019-01-17 08:50:54 +0100

    inline tables from GFMFormatService.kt and todos

--- dokka-hugo/src/main/kotlin/de/cotech/dokka/hugo/HugoFormatService.kt
+++ dokka-hugo/src/main/kotlin/de/cotech/dokka/hugo/HugoFormatService.kt
@@ -5,6 +5,8 @@ import com.google.inject.name.Named
 import org.jetbrains.dokka.*
 import org.jetbrains.dokka.Utilities.impliedPlatformsName
 
+// TODO: prepend _index.md instead of index.md
+
 open class HugoOutputBuilder(to: StringBuilder,
                                location: Location,
                                generator: NodeLocationAwareGenerator,
@@ -23,18 +25,45 @@ open class HugoOutputBuilder(to: StringBuilder,
     protected open fun appendFrontMatter(nodes: Iterable<DocumentationNode>, to: StringBuilder) {
         to.appendln("""title = "${getPageTitle(nodes)}"""")
         to.appendln("""draft = false""")
-        to.appendln("""toc = true""")
+        to.appendln("""toc = false""")
         to.appendln("""type = "javadocs"""")
+        // TODO: only if it's a package and only show package name, not " - hw-security"
         to.appendln("""linktitle = "${getPageTitle(nodes)}"""")
         to.appendln("""[menu.docs]""")
-        to.appendln("""  parent = "Dokka"""")
+        to.appendln("""  parent = "Packages"""")
         to.appendln("""  weight = 1""")
     }
     
+    // Hugo markdown (blackfriday) requires table headers
+    // https://github.com/Kotlin/dokka/blob/master/core/src/main/kotlin/Formats/GFMFormatService.kt
     override fun appendTable(vararg columns: String, body: () -> Unit) {
         to.appendln(columns.joinToString(" | ", "| ", " |"))
         to.appendln("|" + "---|".repeat(columns.size))
         body()
+    }
+
+    override fun appendUnorderedList(body: () -> Unit) {
+        if (inTableCell) {
+            wrapInTag("ul", body)
+        } else {
+            super.appendUnorderedList(body)
+        }
+    }
+
+    override fun appendOrderedList(body: () -> Unit) {
+        if (inTableCell) {
+            wrapInTag("ol", body)
+        } else {
+            super.appendOrderedList(body)
+        }
+    }
+
+    override fun appendListItem(body: () -> Unit) {
+        if (inTableCell) {
+            wrapInTag("li", body)
+        } else {
+            super.appendListItem(body)
+        }
     }
 }
 
@@ -50,7 +79,7 @@ open class HugoFormatService(
             generator: NodeLocationAwareGenerator,
             signatureGenerator: LanguageService,
             @Named(impliedPlatformsName) impliedPlatforms: List<String>
-    ) : this(generator, signatureGenerator, "html", impliedPlatforms)
+    ) : this(generator, signatureGenerator, "md", impliedPlatforms)
 
     override fun createOutputBuilder(to: StringBuilder, location: Location): FormattedOutputBuilder =
             HugoOutputBuilder(to, location, generator, languageService, extension, impliedPlatforms)
